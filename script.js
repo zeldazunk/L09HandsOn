1e0f4c1266eca69b066a026ddb58c281e78e8717
Synetta Faye <synettajenkins@gmail.com>
2018-09-20 10:58:24 -0700

    Trying several different js codes.  Still not working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function einstein() {
+
     let einsteinBio = new XMLHttpRequest();
     einsteinBio.onreadystatechange = function() {
        if (this.readyState == 4 && this.status == 200) {
@@ -10,5 +10,32 @@ function einstein() {
     
     einsteinBio.open("GET", "einstein.json", true);
     einsteinBio.send();
-    }
     
+    
+ 
+    $("MyBtn").click(function(){
+        $.getJSON("script.js", function(result){
+            $.each(result, function( field){
+                $("div").append(field + " " + " " + " " + " ");
+            });
+        });
+    });
+
+
+
+
+
+/*
+let einsteinBio = new XMLHttpRequest();
+newRequest.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    let myObj = JSON.parse(this.responseText);
+    document.getElementById("pic").innerHTML = myObj.pict;
+    document.getElementById("name").innerHTML = myObj.name;
+    document.getElementById("bday").innerHTML = myObj.birthday;
+  }
+};
+newRequest.open("GET", "einstein.json", true);
+newRequest.send();
+*/
+
